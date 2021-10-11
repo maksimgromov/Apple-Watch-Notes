948d2518b7a955524dcb75420cb95912d315221c
Maksim Gromov <mgromov@icloud.com>
2021-10-11 14:45:40 +0700

    Initial Commit

---
+++ Notes WatchKit Extension/NotesApp.swift
@@ -0,0 +1,19 @@
+//
+//  NotesApp.swift
+//  Notes WatchKit Extension
+//
+//  Created by Максим Громов on 11.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct NotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
